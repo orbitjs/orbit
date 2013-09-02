eae752d6558618de68734ab0723c5cdb94ca1180
Dan Gebhardt <dan@cerebris.com>
2013-09-02 12:23:29 -0400

    Introduce `Orbit.assert()`

--- src/orbit/core.js
+++ src/orbit/core.js
@@ -4,4 +4,10 @@ Orbit.required = function() {
   throw new Error('Required property not defined');
 };
 
+Orbit.assert = function(desc, test) {
+  if (!test) {
+    throw new Error("Assertion failed: " + desc);
+  }
+};
+
 export default Orbit;
