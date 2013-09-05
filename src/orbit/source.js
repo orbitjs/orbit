a741301ec7c02d5a2be2033ade994706d9398fe0
Dan Gebhardt <dan@cerebris.com>
2013-09-04 23:27:11 -0400

    Sources must extend Evented

--- src/orbit/source.js
+++ src/orbit/source.js
@@ -1,7 +1,12 @@
+import Orbit from 'orbit/core';
+import Evented from 'orbit/evented';
+
 var Source = function() {
 };
 
 Source.prototype = {
 };
+
+Evented.extend(Source.prototype);
 
 export default Source;
