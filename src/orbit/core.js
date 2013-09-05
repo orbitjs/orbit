fea7dd7114e5feae3244b08512c3253e5e056d92
Dan Gebhardt <dan@cerebris.com>
2013-09-04 23:12:00 -0400

    Add `Array.prototype.forEach` prototype extension if necessary

--- src/orbit/core.js
+++ src/orbit/core.js
@@ -1,3 +1,20 @@
+/**
+ * Prototype extensions
+ */
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function (fn, scope) {
+    var i, len;
+    for (i = 0, len = this.length; i < len; ++i) {
+      if (i in this) {
+        fn.call(scope, this[i], i, this);
+      }
+    }
+  };
+}
+
+/**
+ * Orbit
+ */
 var Orbit = {};
 
 Orbit.required = function() {
