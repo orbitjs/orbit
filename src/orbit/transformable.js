eb681845ce33d6ec1cb5fe000b6f005c3d036614
Dan Gebhardt <dan@cerebris.com>
2013-09-10 23:01:33 -0400

    Filled out Transformable to define `transform()` plus associated events

--- src/orbit/transformable.js
+++ src/orbit/transformable.js
@@ -4,6 +4,28 @@ import Evented from 'orbit/evented';
 var Transformable = {
   extend: function(object) {
     Evented.extend(object);
+
+    object.transform = function() {
+      var args = Array.prototype.slice.call(arguments, 0);
+
+      object.emit.apply(object, ['willTransform'].concat(args));
+
+      Orbit.assert("_transform must be defined", object._transform);
+
+      return object._transform.apply(object, args).then(
+        function(result) {
+          object.emit.apply(object, ['didTransform'].concat(args).concat([result]));
+          object.emit.apply(object, ['afterTransform'].concat(args));
+          return result;
+        },
+        function(result) {
+          object.emit.apply(object, ['didNotTransform'].concat(args).concat([result]));
+          object.emit.apply(object, ['afterTransform'].concat(args));
+          throw result;
+        }
+      );
+    }
+
     return object;
   }
 };
--- tests/unit/transformable_test.js
+++ tests/unit/transformable_test.js
@@ -1,18 +1,128 @@
 import Transformable from 'orbit/transformable';
+import RSVP from 'rsvp';
 
-var transformable;
+var source,
+    target;
+
+var successfulOperation = function() {
+  stop();
+  return new RSVP.Promise(function(resolve, reject) {
+    start();
+    resolve(':)');
+  });
+};
+
+var failedOperation = function() {
+  stop();
+  return new RSVP.Promise(function(resolve, reject) {
+    start();
+    reject(':(');
+  });
+};
+
+///////////////////////////////////////////////////////////////////////////////
 
 module("Unit - Transformable", {
   setup: function() {
-    transformable = {};
-    Transformable.extend(transformable);
+    source = {};
+    target = {};
+    Transformable.extend(source);
+    Transformable.extend(target);
   },
 
   teardown: function() {
-    transformable = null;
+    source = null;
   }
 });
 
 test("it exists", function() {
-  ok(transformable);
+  ok(source);
+});
+
+test("it should mixin Evented", function() {
+  ['on', 'off', 'emit', 'poll'].forEach(function(prop) {
+    ok(source[prop], 'should have Evented properties');
+  })
+});
+
+test("it defines `transform`", function() {
+  ok(source.transform, 'should define `transform`');
+});
+
+test("it requires the definition of `_transform`", function() {
+  throws(source.transform, "presence of `_transform` should be verified");
+});
+
+test("`transform` returns the value of `_transform`", function() {
+  source._transform = successfulOperation;
+  source.transform().then(function(result) {
+    equal(result, ':)', 'tranform returns the same value as _tranform');
+  });
+});
+
+test("it should emit `willTransform` and `didTransform` events for a successful transform", function() {
+  expect(9);
+
+  var order = 0;
+
+  source.on('willTransform', function() {
+    equal(++order, 1, 'willTransform emitted first');
+    deepEqual(toArray(arguments), ['abc', 'def'], 'event handler args match original call args');
+  });
+
+  source._transform = function() {
+    equal(++order, 2, '_transform called next');
+    deepEqual(toArray(arguments), ['abc', 'def'], '_transform args match original call args');
+    return successfulOperation();
+  };
+
+  source.on('didTransform', function() {
+    equal(++order, 3, 'didTransform emitted after transform performed successfully');
+    deepEqual(toArray(arguments), ['abc', 'def', ':)'], 'event handler args match original call args + return value');
+  });
+
+  source.on('afterTransform', function() {
+    equal(++order, 4, 'afterTransform emitted after didTransform');
+  });
+
+  source.transform.call(source, 'abc', 'def').then(function(result) {
+    equal(++order, 5, 'promise resolved last');
+    equal(result, ':)', 'success!');
+  });
+});
+
+test("it should emit `willTransform` and `didNotTransform` events for an unsuccessful transform", function() {
+  expect(10);
+
+  var order = 0;
+
+  source.on('willTransform', function() {
+    equal(++order, 1, 'willTransform emitted first');
+    deepEqual(toArray(arguments), ['abc', 'def'], 'event handler args match original call args');
+  });
+
+  source._transform = function() {
+    equal(++order, 2, '_transform called next');
+    deepEqual(toArray(arguments), ['abc', 'def'], '_transform args match original call args');
+    return failedOperation();
+  };
+
+  source.on('didTransform', function() {
+    ok(false, 'didTransform should not be triggered');
+  });
+
+  source.on('didNotTransform', function() {
+    equal(++order, 3, 'didNotTransform emitted after transform could not be performed successfully');
+    deepEqual(toArray(arguments), ['abc', 'def', ':('], 'event handler args match original call args + return value');
+  });
+
+  source.on('afterTransform', function() {
+    equal(++order, 4, 'afterTransform emitted after didNotTransform');
+    deepEqual(toArray(arguments), ['abc', 'def'], 'event handler args match original call args');
+  });
+
+  source.transform.call(source, 'abc', 'def').then(null, function(result) {
+    equal(++order, 5, 'promise resolved last');
+    equal(result, ':(', 'failure');
+  });
 });
