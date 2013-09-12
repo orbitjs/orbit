56cf06666e59b3c5b8f8fca63f74fc3d8cae0cfa
Dan Gebhardt <dan@cerebris.com>
2013-09-11 21:50:21 -0400

    Introduce MemoryStore

---
+++ src/orbit/sources/memory_store.js
@@ -0,0 +1,113 @@
+import Orbit from 'orbit/core';
+import Transformable from 'orbit/transformable';
+import Requestable from 'orbit/requestable';
+import RSVP from 'rsvp';
+
+var NOT_FOUND = 'Record not found';
+
+var MemoryStore = function() {
+  this._data = {};
+  this.length = 0;
+  this._newId = 0;
+
+  Transformable.extend(this);
+  Requestable.extend(this, ['find']);
+};
+
+var updateRecord = function(record, data) {
+  for (var i in data) {
+    if (data.hasOwnProperty(i)) {
+      record[i] = data[i];
+    }
+  }
+  for (i in record) {
+    if (data.hasOwnProperty(i) && data[i] === undefined) {
+      delete record[i];
+    }
+  }
+};
+
+var patchRecord = function(record, data) {
+  for (var i in data) {
+    if (data.hasOwnProperty(i)) {
+      record[i] = data[i];
+    }
+  }
+};
+
+MemoryStore.prototype = {
+  constructor: MemoryStore,
+
+  _transform: function(operation, data) {
+    var _this = this;
+
+    return new RSVP.Promise(function(resolve, reject) {
+      var record;
+
+      if (operation === 'insert') {
+        data.id = _this._generateId();
+        _this._data[data.id] = data;
+        _this.length++;
+        resolve(data);
+
+      } else if (operation === 'update') {
+        record = _this._data[data.id];
+        if (record) {
+          updateRecord(record, data);
+          resolve(record);
+        } else {
+          reject(NOT_FOUND);
+        }
+
+      } else if (operation === 'patch') {
+        record = _this._data[data.id];
+        if (record) {
+          patchRecord(record, data);
+          resolve(record);
+        } else {
+          reject(NOT_FOUND);
+        }
+
+      } else if (operation === 'destroy') {
+        record = _this._data[data.id];
+        if (record) {
+          delete _this._data[data.id];
+          _this.length--;
+          resolve();
+        } else {
+          reject(NOT_FOUND);
+        }
+      }
+    });
+
+  },
+
+  _generateId: function() {
+    this._newId++;
+    return this._newId;
+  },
+
+  _find: function(id) {
+    var _this = this;
+    return new RSVP.Promise(function(resolve, reject) {
+      if (id === undefined) {
+        var all = [];
+        for (var i in _this._data) {
+          if (_this._data.hasOwnProperty(i)) {
+            all.push(_this._data[i]);
+          }
+        }
+        resolve(all);
+      } else {
+        var record = _this._data[id];
+        if (record) {
+          resolve(record);
+        } else {
+          reject(NOT_FOUND);
+        }
+      }
+    });
+  }
+};
+
+export default MemoryStore;
---
+++ tests/unit/sources/memory_store_test.js
@@ -0,0 +1,133 @@
+import MemoryStore from 'orbit/sources/memory_store';
+import RSVP from 'rsvp';
+
+var dogs;
+
+///////////////////////////////////////////////////////////////////////////////
+
+module("Unit - MemoryStore", {
+  setup: function() {
+    dogs = new MemoryStore();
+  },
+
+  teardown: function() {
+    dogs = null;
+  }
+});
+
+test("it exists", function() {
+  ok(dogs);
+});
+
+test("it can insert records and assign ids", function() {
+  expect(6);
+
+  equal(dogs.length, 0, 'store should be empty');
+
+  stop();
+  dogs.transform('insert', {name: 'Hubert', gender: 'm'}).then(function(dog) {
+    equal(dogs.length, 1, 'store should contain one record');
+    ok(dog.id, 'id should be defined');
+    equal(dog.name, 'Hubert', 'name should match');
+    equal(dog.gender, 'm', 'gender should match');
+    return dog;
+
+  }).then(function(dog) {
+    dogs.find(dog.id).then(function(foundDog) {
+      start();
+      equal(foundDog.id, dog.id, 'record can be looked up by id');
+    });
+  });
+});
+
+test("it can update records", function() {
+  expect(8);
+
+  equal(dogs.length, 0, 'store should be empty');
+
+  var original;
+
+  stop();
+  dogs.transform('insert', {name: 'Hubert', gender: 'm'}).then(function(dog) {
+    original = dog;
+    dogs.transform('update', {id: dog.id, name: 'Beatrice', gender: 'f'}).then(function(updatedDog) {
+      equal(updatedDog.id, dog.id, 'id remains the same');
+      equal(updatedDog.name, 'Beatrice', 'name has been updated');
+      equal(updatedDog.gender, 'f', 'gender has been updated');
+    })
+    return dog;
+
+  }).then(function(dog) {
+    dogs.find(dog.id).then(function(foundDog) {
+      start();
+      strictEqual(foundDog, original, 'still the same object as the one originally inserted');
+      equal(foundDog.id, dog.id, 'record can be looked up by id');
+      equal(foundDog.name, 'Beatrice', 'name has been updated');
+      equal(foundDog.gender, 'f', 'gender has been updated');
+    });
+  });
+});
+
+test("it can patch records", function() {
+  expect(8);
+
+  equal(dogs.length, 0, 'store should be empty');
+
+  var original;
+
+  stop();
+  dogs.transform('insert', {name: 'Hubert', gender: 'm'}).then(function(dog) {
+    original = dog;
+    dogs.transform('patch', {id: dog.id, name: 'Beatrice'}).then(function(updatedDog) {
+      equal(updatedDog.id, dog.id, 'id remains the same');
+      equal(updatedDog.name, 'Beatrice', 'name has been updated');
+      equal(updatedDog.gender, 'm', 'gender has not been updated');
+    })
+    return dog;
+
+  }).then(function(dog) {
+    dogs.find(dog.id).then(function(foundDog) {
+      start();
+      strictEqual(foundDog, original, 'still the same object as the one originally inserted');
+      equal(foundDog.id, dog.id, 'record can be looked up by id');
+      equal(foundDog.name, 'Beatrice', 'name has been updated');
+      equal(foundDog.gender, 'm', 'gender has not been updated');
+    });
+  });
+});
+
+test("it can destroy records", function() {
+  expect(3);
+
+  equal(dogs.length, 0, 'store should be empty');
+
+  stop();
+  dogs.transform('insert', {name: 'Hubert', gender: 'm'}).then(function(dog) {
+    equal(dogs.length, 1, 'store should contain one record');
+
+    dogs.transform('destroy', {id: dog.id}).then(function() {
+      start();
+      equal(dogs.length, 0, 'store should be empty');
+    })
+  });
+});
+
+test("it can find all records", function() {
+  expect(3);
+
+  equal(dogs.length, 0, 'store should be empty');
+
+  stop();
+  RSVP.all([
+    dogs.transform('insert', {name: 'Hubert', gender: 'm'}),
+    dogs.transform('insert', {name: 'Beatrice', gender: 'f'}),
+    dogs.transform('insert', {name: 'Winky', gender: 'm'})
+  ]).then(function() {
+    equal(dogs.length, 3, 'store should contain 3 dogs');
+
+    dogs.find().then(function(allDogs) {
+      start();
+      equal(allDogs.length, 3, 'find() should return all records');
+    });
+  });
+});
