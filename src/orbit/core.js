2cffa7fdf8ec6c2fe4878e21982592a098e1cf34
Dan Gebhardt <dan@cerebris.com>
2013-09-26 22:19:18 -0400

    New to `Orbit`: error msgs, `idField`, `generateId`, `clone`

--- src/orbit/core.js
+++ src/orbit/core.js
@@ -15,23 +15,68 @@ if (!Array.prototype.forEach) {
 /**
  * Orbit
  */
-var Orbit = {};
+var Orbit = {
+  NOT_FOUND: 'Record not found',
+  ALREADY_EXISTS: 'Record already exists',
 
-Orbit.assert = function(desc, test) {
-  if (!test) {
-    throw new Error("Assertion failed: " + desc);
+  idField: '__id',
+
+  generateId: function() {
+    if (this._newId) {
+      this._newId++;
+    } else {
+      this._newId = 1;
+    }
+    return new Date().getTime() + '-' + this._newId;
+  },
+
+  assert: function(desc, test) {
+    if (!test) throw new Error("Assertion failed: " + desc);
+  },
+
+  capitalize: function(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+
+  toArray: function(obj, startingIndex) {
+    return Array.prototype.slice.call(obj, startingIndex || 0);
+  },
+
+  clone: function(obj) {
+    if (obj === undefined || obj === null || typeof obj !== 'object') return obj;
+
+    var dup,
+        type = Object.prototype.toString.call(obj);
+
+    if (type === "[object Date]") {
+      dup = new Date();
+      dup.setTime(obj.getTime());
+
+    } else if (type === "[object RegExp]") {
+      dup = obj.constructor(obj);
+
+    } else if (type === "[object Array]") {
+      dup = [];
+      for (var i = 0, len = obj.length; i < len; i++) {
+        if (obj.hasOwnProperty(i)) {
+          dup.push(this.clone(obj[i]));
+        }
+      }
+
+    } else  {
+      var val;
+
+      dup = {};
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          val = obj[key];
+          if (typeof val === 'object') val = this.clone(val);
+          dup[key] = val;
+        }
+      }
+    }
+    return dup;
   }
 };
-
-Orbit.capitalize = function(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
-
-Orbit.toArray = function(obj, startingIndex) {
-  return Array.prototype.slice.call(obj, startingIndex || 0);
-};
-
-Orbit.NOT_FOUND = 'Record not found';
-Orbit.ALREADY_EXISTS = 'Record already exists';
 
 export default Orbit;
--- tests/unit/core_test.js
+++ tests/unit/core_test.js
@@ -9,7 +9,31 @@ test("it exists", function() {
   ok(Orbit);
 });
 
-test("capitalize capitalizes the first letter of a word", function() {
-  equal(Orbit.capitalize("cauliflower"), "Cauliflower", "capitalize capitalizes the first letter of a word");
-  equal(Orbit.capitalize("aSAP"), "ASAP", "capitalize doesn't touch the rest of the word");
+test("#idField defines a default id field", function() {
+  equal(Orbit.idField, '__id', 'default internal id field');
 });
+
+test("#generateId generates unique ids", function() {
+  notEqual(Orbit.generateId(), Orbit.generateId(), 'a weak test to ensure uniqueness');
+});
+
+test("#capitalize capitalizes the first letter of a word", function() {
+  equal(Orbit.capitalize('cauliflower'), 'Cauliflower', 'capitalize capitalizes the first letter of a word');
+  equal(Orbit.capitalize('aSAP'), 'ASAP', 'capitalize doesn\'t touch the rest of the word');
+});
+
+test("#clone creates a deep clone of an object's own properties", function() {
+  var obj = {
+    a: 1,
+    b: '2',
+    c: ["3", {d: "4", e: ["5", "6"]}, 7],
+    f: new Date(),
+    g: /123/g
+  };
+
+  var copy = Orbit.clone(obj);
+
+  deepEqual(obj, copy, 'clone is deeply equal to original');
+  notStrictEqual(obj, copy, 'clone is not strictly equal to original');
+});
+
