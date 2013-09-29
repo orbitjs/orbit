325582b5d6ba13e6c82eec46ca7739cba10edce0
Dan Gebhardt <dan@cerebris.com>
2013-09-29 11:09:17 -0400

    Introduce `async`, `queueEnabled` and `active` options to `TransformConnector`

--- src/orbit/connectors/transform_connector.js
+++ src/orbit/connectors/transform_connector.js
@@ -27,37 +27,124 @@ var patchIfNecessary = function(record, otherwise) {
   }
 };
 
-var TransformConnector = function(source, target) {
+var TransformConnector = function(source, target, options) {
   var _this = this;
 
   this.source = source;
   this.target = target;
-  this.autosync = true;
   this.queue = [];
 
-  _this.source.on('didInsertRecord', function(data, record) {
-    console.log('inserted', _this.source, record);
-    return patchIfNecessary.call(_this, record, function() {
+  options = options || {};
+  this._async = options['async'] !== undefined ? options['async'] : false;
+  this._queueEnabled = options['queue'] !== undefined ? options['queue'] : false;
+  var active = options['active'] !== undefined ? options['active'] : true;
+  if (active) this.activate();
+};
+
+TransformConnector.prototype = {
+  constructor: TransformConnector,
+
+  activate: function() {
+    var _this = this;
+
+    if (this._active) return;
+
+    this.source.on('didInsertRecord',  this._onInsert, this);
+    this.source.on('didUpdateRecord',  this._onUpdate, this);
+    this.source.on('didPatchRecord',   this._onPatch, this);
+    this.source.on('didDestroyRecord', this._onDestroy, this);
+
+    this._active = true;
+  },
+
+
+  deactivate: function() {
+    this.source.off('didInsertRecord',  this._onInsert, this);
+    this.source.off('didUpdateRecord',  this._onUpdate, this);
+    this.source.off('didPatchRecord',   this._onPatch, this);
+    this.source.off('didDestroyRecord', this._onDestroy, this);
+    this._active = false;
+  },
+
+  isActive: function() {
+    return this._active;
+  },
+
+  enableAsync: function() {
+    this._async = true;
+  },
+
+  disableAsync: function() {
+    this._async = false;
+  },
+
+  isAsync: function() {
+    return this._async;
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Internals
+  /////////////////////////////////////////////////////////////////////////////
+
+  _onInsert: function(data, record) {
+    if (this._queueEnabled) {
+      this.queue.push({type: 'insert', record: record});
+    } else {
+      return this._handleInsert(record);
+    }
+  },
+
+  _onUpdate: function(data, record) {
+    if (this._queueEnabled) {
+      this.queue.push({type: 'update', record: record});
+    } else {
+      return this._handleUpdate(record);
+    }
+  },
+
+  _onPatch: function(data, record) {
+    if (this._queueEnabled) {
+      this.queue.push({type: 'patch', record: record});
+    } else {
+      return this._handlePatch(record);
+    }
+  },
+
+  _onDestroy: function(data, record) {
+    if (this._queueEnabled) {
+      this.queue.push({type: 'destroy', record: record});
+    } else {
+      return this._handleDestroy(record);
+    }
+  },
+
+  _handleInsert: function(record) {
+    var _this = this;
+    var promise = patchIfNecessary.call(_this, record, function() {
       return _this.target.insertRecord(record).then(null, failHandler);
     });
-  }, failHandler);
+    if (!this._async) return promise;
+  },
 
-  _this.source.on('didUpdateRecord', function(data, record) {
-    console.log('updated', _this.source, record);
-    return patchIfNecessary.call(_this, record, function() {
+  _handleUpdate: function(record) {
+    var _this = this;
+    var promise = patchIfNecessary.call(_this, record, function() {
       return _this.target.updateRecord(record).then(null, failHandler);
     });
-  }, failHandler);
+    if (!this._async) return promise;
+  },
 
-  _this.source.on('didPatchRecord', function(data, record) {
-    console.log('patched', _this.source, record);
-    return patchIfNecessary.call(_this, record, function() {
+  _handlePatch: function(record) {
+    var _this = this;
+    var promise = patchIfNecessary.call(_this, record, function() {
       return _this.target.patchRecord(record).then(null, failHandler);
     });
-  }, failHandler);
+    if (!this._async) return promise;
+  },
 
-  _this.source.on('didDestroyRecord', function(data, record) {
-    console.log('destroyed', _this.source, record);
+  _handleDestroy: function(record) {
+    var _this = this,
+        promise;
 
     // attempt to retrieve target record to determine whether it needs to be deleted
     var targetRecordExists = true;
@@ -69,11 +156,13 @@ var TransformConnector = function(source, target) {
     if (targetRecordExists) {
       return _this.target.destroyRecord(record).then(null, failHandler);
     }
-  }, failHandler);
-};
 
-TransformConnector.prototype = {
-  constructor: TransformConnector
+    promise = patchIfNecessary.call(_this, record, function() {
+      return _this.target.destroyRecord(record).then(null, failHandler);
+    });
+
+    if (!this._async) return promise;
+  }
 };
 
 export default TransformConnector;
