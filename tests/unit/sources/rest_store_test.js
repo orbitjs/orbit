122e486c086336f4b84dfa582a55fb60e0fa0e86
Dan Gebhardt <dan@cerebris.com>
2013-10-10 22:18:36 -0400

    Fill out `find` tests for `RestStore`

--- tests/unit/sources/rest_store_test.js
+++ tests/unit/sources/rest_store_test.js
@@ -107,7 +107,7 @@ test("it can delete records", function() {
   });
 });
 
-test("it can find individual records", function() {
+test("it can find individual records by passing in a single id", function() {
   expect(6);
 
   server.respondWith('POST', '/planets', function(xhr) {
@@ -136,24 +136,97 @@ test("it can find individual records", function() {
   });
 });
 
-// TODO
-//test("it can find all records", function() {
-//  expect(1);
-//
-//  var response = [
-//    {id: 12345, name: 'Jupiter', classification: 'gas giant'},
-//    {id: 12346, name: 'Earth', classification: 'terrestrial'}
-//  ];
-//
-//  server.respondWith('GET', '/planets', function(xhr) {
-//    xhr.respond(200,
-//                {'Content-Type': 'application/json'},
-//                JSON.stringify(response));
-//  });
-//
-//  stop();
-//  store.find('planet').then(function(planets) {
-//    start();
-//    deepEqual(planets, response, 'data matches response');
-//  });
-//});
+test("it can find individual records by passing in a single remote id", function() {
+  expect(6);
+
+  server.respondWith('POST', '/planets', function(xhr) {
+    deepEqual(JSON.parse(xhr.requestBody), {name: 'Jupiter', classification: 'gas giant'}, 'POST request');
+    xhr.respond(201,
+                {'Content-Type': 'application/json'},
+                JSON.stringify({id: 12345, name: 'Jupiter', classification: 'gas giant'}));
+  });
+
+  server.respondWith('GET', '/planets/12345', function(xhr) {
+    ok(true, 'GET request');
+    xhr.respond(200,
+                {'Content-Type': 'application/json'},
+                JSON.stringify({id: 12345, name: 'Jupiter', classification: 'gas giant'}));
+  });
+
+  stop();
+  store.insertRecord('planet', {name: 'Jupiter', classification: 'gas giant'}).then(function(planet) {
+    store.find('planet', {id: planet.id}).then(function(planet) {
+      start();
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, 12345, 'server id should be defined');
+      equal(planet.name, 'Jupiter', 'name should match');
+      equal(planet.classification, 'gas giant', 'classification should match');
+    });
+  });
+});
+
+test("it can find all records", function() {
+  expect(13);
+
+  var records = [
+    {id: 1, name: 'Jupiter', classification: 'gas giant'},
+    {id: 2, name: 'Earth', classification: 'terrestrial'},
+    {id: 3, name: 'Saturn', classification: 'gas giant'}
+  ];
+
+  server.respondWith('GET', '/planets', function(xhr) {
+    ok(true, 'GET request');
+    xhr.respond(200,
+                {'Content-Type': 'application/json'},
+                JSON.stringify(records));
+  });
+
+  stop();
+  store.find('planet').then(function(planets) {
+    start();
+
+    var planet, record;
+    for (var i = 0; i < planets.length; i++) {
+      planet = planets[i];
+      record = records[i];
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, record.id, 'server id should be defined');
+      equal(planet.name, record.name, 'name should match');
+      equal(planet.classification, record.classification, 'classification should match');
+    }
+  });
+});
+
+test("it can find all records", function() {
+  expect(18);
+
+  var records = [
+    {id: 1, name: 'Mercury', classification: 'terrestrial'},
+    {id: 2, name: 'Venus', classification: 'terrestrial'},
+    {id: 3, name: 'Earth', classification: 'terrestrial'},
+    {id: 4, name: 'Mars', classification: 'terrestrial'}
+  ];
+
+  server.respondWith(function(xhr) {
+    equal(xhr.method, 'GET', 'GET request');
+    equal(xhr.url, '/planets?classification=terrestrial', 'request to correct URL');
+    xhr.respond(200,
+                {'Content-Type': 'application/json'},
+                JSON.stringify(records));
+  });
+
+  stop();
+  store.find('planet', {classification: 'terrestrial'}).then(function(planets) {
+    start();
+
+    var planet, record;
+    for (var i = 0; i < planets.length; i++) {
+      planet = planets[i];
+      record = records[i];
+      ok(planet.__id, 'orbit id should be defined');
+      equal(planet.id, record.id, 'server id should be defined');
+      equal(planet.name, record.name, 'name should match');
+      equal(planet.classification, record.classification, 'classification should match');
+    }
+  });
+});
