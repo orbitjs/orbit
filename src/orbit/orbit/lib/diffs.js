391295c4a7b63cd8367dc3ab568f9ebe5aac3e51
Dan Gebhardt <dan@cerebris.com>
2013-11-17 22:58:02 -0500

    Add special handling for arrays in `diffs`

--- src/orbit/orbit/lib/diffs.js
+++ src/orbit/orbit/lib/diffs.js
@@ -28,24 +28,70 @@ var diffs = function(a, b, options) {
         var i,
             d;
 
-        for (i in b) {
-          if (!ignore[i]) {
-            if (a[i] === undefined) {
-              if (d === undefined) d = [];
-              d.push({op: 'add', path: basePath + '/' + i, value: clone(b[i])});
+        if (type === '[object Array]') {
+          var aLength = a.length,
+              bLength = b.length,
+              maxLength = bLength > aLength ? bLength : aLength,
+              match,
+              ai = 0,
+              bi = 0,
+              bj;
 
-            } else if (!eq(a[i], b[i])) {
+          for (i = 0; i < maxLength; i++) {
+            if (ai >= aLength) {
               if (d === undefined) d = [];
-              d = d.concat(diffs(a[i], b[i], {basePath: basePath + '/' + i}));
+              d.push({op: 'add', path: basePath + '/' + bi, value: clone(b[bi])});
+              bi++;
+
+            } else if (bi >= bLength) {
+              if (d === undefined) d = [];
+              d.push({op: 'remove', path: basePath + '/' + ai});
+              ai++;
+
+            } else if (!eq(a[ai], b[bi])) {
+              match = -1;
+              for (bj = bi + 1; bj < bLength; bj++) {
+                if (eq(a[ai], b[bj])) {
+                  match = bj;
+                  break;
+                }
+              }
+              if (match === -1) {
+                if (d === undefined) d = [];
+                d.push({op: 'remove', path: basePath + '/' + ai});
+                ai++;
+
+              } else {
+                if (d === undefined) d = [];
+                d.push({op: 'add', path: basePath + '/' + bi, value: clone(b[bi])});
+                bi++;
+              }
+            } else {
+              ai++;
+              bi++;
             }
           }
-        }
 
-        for (i in a) {
-          if (!ignore[i]) {
-            if (b[i] === undefined) {
-              if (d === undefined) d = [];
-              d.push({op: 'remove', path: basePath + '/' + i});
+        } else { // general (non-array) object
+          for (i in b) {
+            if (!ignore[i] && b.hasOwnProperty(i)) {
+              if (a[i] === undefined) {
+                if (d === undefined) d = [];
+                d.push({op: 'add', path: basePath + '/' + i, value: clone(b[i])});
+
+              } else if (!eq(a[i], b[i])) {
+                if (d === undefined) d = [];
+                d = d.concat(diffs(a[i], b[i], {basePath: basePath + '/' + i}));
+              }
+            }
+          }
+
+          for (i in a) {
+            if (!ignore[i] && a.hasOwnProperty(i)) {
+              if (b[i] === undefined) {
+                if (d === undefined) d = [];
+                d.push({op: 'remove', path: basePath + '/' + i});
+              }
             }
           }
         }
@@ -56,6 +102,7 @@ var diffs = function(a, b, options) {
         return undefined;
       }
     }
+
     return [{op: 'replace', path: basePath, value: clone(b)}];
   }
 };
--- tests/unit/lib/diffs_test.js
+++ tests/unit/lib/diffs_test.js
@@ -27,20 +27,46 @@ test("#diffs return `undefined` for matching objects", function() {
   deepEqual(diffs(a, b), undefined);
 });
 
-test("#diffs creates an array of patches when comparing two objects", function() {
+test("#diffs generates `add` patches when comparing two objects (as per rfc 6902)", function() {
   var a, b;
 
+  // RFC 6902 - Appendix A.1.  Adding an Object Member
+  a = {foo: 'bar'};
+  b = {foo: 'bar', 'baz': 'qux'};
+
+  deepEqual(diffs(a, b),
+            [{op: 'add', path: '/baz', value: 'qux'}]);
+
+  // RFC 6902 - Appendix A.2.  Adding an Array Element
+  a = {foo: ['bar', 'baz']};
+  b = {foo: ['bar', 'qux', 'baz']};
+
+  deepEqual(diffs(a, b),
+            [{op: 'add', path: '/foo/1', value: 'qux'}]);
+
+  // Adding multiple array elements
+  a = {foo: ['BAR', 'BAZ']};
+  b = {foo: ['BAR', 'qux', 'dux', 'BAZ']};
+
+  deepEqual(diffs(a, b),
+            [{op: 'add', path: '/foo/1', value: 'qux'},
+             {op: 'add', path: '/foo/2', value: 'dux'}]);
+
+  // Adding multiple array elements
+  a = {foo: ['BAR', 'BAZ']};
+  b = {foo: ['BAR', 'qux', 'dux', 'BAZ', 'flux']};
+
+  deepEqual(diffs(a, b),
+            [{op: 'add', path: '/foo/1', value: 'qux'},
+             {op: 'add', path: '/foo/2', value: 'dux'},
+             {op: 'add', path: '/foo/4', value: 'flux'}]);
+
+  // Misc tests
   a = {name: 'Jupiter', classification: 'gas giant'};
   b = {id: 12345, name: 'Jupiter', classification: 'gas giant'};
 
   deepEqual(diffs(a, b, {basePath: 'planets/1'}),
             [{op: 'add', path: 'planets/1/id', value: 12345}]);
-
-  a = {id: 12345, name: 'Jupiter', classification: 'gas giant'};
-  b = {name: 'Jupiter', classification: 'gas giant'};
-
-  deepEqual(diffs(a, b, {basePath: 'planets/1'}),
-            [{op: 'remove', path: 'planets/1/id'}]);
 
   a = {name: 'Jupiter', classification: 'gas giant'};
   b = {id: {a: '1', b: '2'}, name: 'Jupiter', classification: 'gas giant'};
@@ -48,6 +74,37 @@ test("#diffs creates an array of patches when comparing two objects", function()
   deepEqual(diffs(a, b, {basePath: 'planets/1'}),
             [{op: 'add', path: 'planets/1/id', value: {a: '1', b: '2'}}]);
 
+});
+
+test("#diffs generates `remove` patches when comparing two objects (as per rfc 6902)", function() {
+  var a, b;
+
+  // RFC 6902 - Appendix A.4. Removing an Array Element
+  a = {foo: ['bar', 'qux', 'baz']};
+  b = {foo: ['bar', 'baz']};
+
+  deepEqual(diffs(a, b),
+            [{op: 'remove', path: '/foo/1'}]);
+
+  // Misc tests
+  a = {id: 12345, name: 'Jupiter', classification: 'gas giant'};
+  b = {name: 'Jupiter', classification: 'gas giant'};
+
+  deepEqual(diffs(a, b, {basePath: 'planets/1'}),
+            [{op: 'remove', path: 'planets/1/id'}]);
+});
+
+test("#diffs generates `replace` patches when comparing two objects (as per rfc 6902)", function() {
+  var a, b;
+
+  // RFC 6902 - Appendix A.5. Replacing a Value
+  a = {foo: 'bar', baz: 'qux'};
+  b = {foo: 'bar', baz: 'boo'};
+
+  deepEqual(diffs(a, b),
+            [{op: 'replace', path: '/baz', 'value': 'boo'}]);
+
+  // Misc tests
   a = {id: {a: '1', b: '2'}, name: 'Jupiter', classification: 'gas giant'};
   b = {id: {a: '1', b: '3'}, name: 'Jupiter', classification: 'gas giant'};
 
