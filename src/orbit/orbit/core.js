e1b2c30073510b8c290147d125bfb4d21aaa5ec6
Dan Gebhardt <dan@cerebris.com>
2013-12-01 14:27:52 -0500

    Fix `incrementVersion`

--- src/orbit/orbit/core.js
+++ src/orbit/orbit/core.js
@@ -33,7 +33,7 @@ var Orbit = {
   versionField: '__ver',
 
   incrementVersion: function(record) {
-    if (record[this.idField]) {
+    if (record[this.versionField] || record[this.idField] === undefined) {
       record[this.versionField] = this.generateId();
     } else {
       record[this.versionField] = record[this.idField];
