ebb79ab1c5693c419c1867b082e88d0478db79e3
Dan Gebhardt <dan@cerebris.com>
2014-01-25 15:58:09 -0500

    Extract `generateId` from main module

--- test/tests/orbit/unit/main_test.js
+++ test/tests/orbit/unit/main_test.js
@@ -9,10 +9,6 @@ test("it exists", function() {
   ok(Orbit);
 });
 
-test("#generateId generates unique ids", function() {
-  notEqual(Orbit.generateId(), Orbit.generateId(), 'a weak test to ensure uniqueness');
-});
-
 test("#capitalize capitalizes the first letter of a word", function() {
   equal(Orbit.capitalize('cauliflower'), 'Cauliflower', 'capitalize capitalizes the first letter of a word');
   equal(Orbit.capitalize('aSAP'), 'ASAP', 'capitalize doesn\'t touch the rest of the word');
