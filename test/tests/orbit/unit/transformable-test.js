4e13b2b430baa9a5f0acb13dae95de64cda90c4b
Corey Frang <corey@bocoup.com>
2014-11-06 13:59:29 -0500

    Single settle loop

--- test/tests/orbit/unit/transformable-test.js
+++ test/tests/orbit/unit/transformable-test.js
@@ -117,3 +117,55 @@ test("it should trigger `didTransform` event after a successful transform", func
     equal(++order, 3, 'promise resolved last');
   });
 });
+
+
+test("it should wait for the current settle loop before starting another", function() {
+  expect(11);
+
+  var order = 0,
+      addOp = {op: 'add', path: 'planet/1', value: 'data'},
+      inverseOp = {op: 'remove', path: 'planet/1'};
+
+  // though this is definitely an awkward use case, it definitely ensures execution order
+  // is what we want it to be
+  source._transform = function(operation) {
+    if (operation.op === 'add') {
+      equal(++order, 1, '_transform add performed first');
+      this.didTransform(addOp, inverseOp);
+      this.settleTransforms().then(function() {
+        equal(++order, 7, 'settleTransforms while transforming called after other transforms');
+      });
+    }
+    if (operation.op === 'remove') {
+      equal(++order, 3, '_transform remove performed second');
+      this.didTransform(inverseOp, {order: 4});
+      this.didTransform(inverseOp, {order: 5});
+      this.didTransform(inverseOp, {order: 6});
+      this.settleTransforms().then(function() {
+        equal(++order, 8, 'settleTransforms while transforming called after other transforms');
+      });
+      this.settleTransforms().then(function() {
+        equal(++order, 9, 'settleTransforms while transforming called after other transforms');
+      });
+    }
+    return successfulOperation();
+  };
+
+  Transformable.extend(source);
+
+  source.on('didTransform', function(operation, inverse) {
+    if (operation.op === 'add') {
+      equal(++order, 2, 'didTransform triggered after action performed successfully');
+      deepEqual(operation, addOp, 'operation matches');
+    }
+    if (operation.op === 'remove') {
+      equal(++order, inverse.order, 'didTransform triggered after action');
+    }
+  });
+
+  stop();
+  source.transform([addOp, inverseOp]).then(function() {
+    start();
+    equal(++order, 10, 'promise resolved last');
+  });
+});
