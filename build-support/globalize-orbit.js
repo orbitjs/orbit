3e0df1f13c5b28021a998f12a1db658b94e94408
Dan Gebhardt <dan@cerebris.com>
2015-03-13 22:55:11 -0400

    Orbit.__require__ and Orbit.__requirejs__ no longer required.

--- build-support/globalize-orbit.js
+++ build-support/globalize-orbit.js
@@ -3,7 +3,5 @@ var Orbit = requireModule("orbit");
 // Globalize loader properties for use by other Orbit packages
 Orbit.__define__ = define;
 Orbit.__requireModule__ = requireModule;
-Orbit.__require__ = require;
-Orbit.__requirejs__ = requirejs;
 
 window.Orbit = Orbit;
--- build-support/globalized-loader.js
+++ build-support/globalized-loader.js
@@ -1,5 +1,3 @@
 // Share loader properties from globalized Orbit package
 var define = window.Orbit.__define__;
 var requireModule = window.Orbit.__requireModule__;
-var require = window.Orbit.__require__;
-var requirejs = window.Orbit.__requirejs__;
