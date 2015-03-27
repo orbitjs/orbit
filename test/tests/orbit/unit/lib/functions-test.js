40af0019205df82dd64b05d01a5867c7ffc63e6a
opsb <oliver@opsb.co.uk>
2015-03-26 22:57:32 -0600

    Add `spread` function to orbit/lib/objects

---
+++ test/tests/orbit/unit/lib/functions-test.js
@@ -0,0 +1,16 @@
+import { spread } from 'orbit/lib/functions';
+
+module("Orbit - Lib - Functions", {
+});
+
+test("`spread` wraps a function that expects parameters with another that can accept the parameters as an array", function() {
+  expect(3);
+
+  var f = function(a, b, c) {
+    equal(a, 'a');
+    equal(b, 'b');
+    equal(c, 'c');
+  };
+
+  spread(f)(['a', 'b', 'c']);
+});
