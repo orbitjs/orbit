110a988a7ab159ce69a502c955b1b26a175bc37f
Dan Gebhardt <dan@cerebris.com>
2015-08-07 10:14:10 -0400

    Enhance Transform with operation normalization and `isEmpty` method.

--- test/tests/orbit/unit/transform-test.js
+++ test/tests/orbit/unit/transform-test.js
@@ -1,22 +1,46 @@
 import Orbit from 'orbit/main';
 import Transform from 'orbit/transform';
-import { Promise } from 'rsvp';
+import Operation from 'orbit/operation';
+import { equalOps } from 'tests/test-helper';
 
 ///////////////////////////////////////////////////////////////////////////////
 
 module("Orbit - Transform", {
-  setup: function() {
-    Orbit.Promise = Promise;
-  },
-
-  teardown: function() {
-    Orbit.Promise = null;
-  }
 });
 
 test("it exists", function() {
   var transform = new Transform();
   ok(transform);
+});
+
+test("it normalizes its operations", function() {
+  expect(3);
+
+  var transform = new Transform([
+    {op: 'add', path: 'planet/1', value: {id: '1'}}
+  ]);
+
+  transform.push([
+    {op: 'add', path: 'planet/2', value: {id: '2'}}
+  ]);
+
+  equal(transform.operations.length, 2);
+  ok(transform.operations[0] instanceof Operation);
+  ok(transform.operations[1] instanceof Operation);
+});
+
+test("#isEmpty returns true if no operations have been added", function() {
+  expect(2);
+
+  var transform = new Transform();
+
+  equal(transform.isEmpty(), true);
+
+  transform.push([
+    {op: 'add', path: 'planet/2', value: {id: '2'}}
+  ]);
+
+  equal(transform.isEmpty(), false);
 });
 
 test("it is assigned an `id`", function() {
@@ -78,7 +102,6 @@ test("can be created from with all attributes specified as options", function() 
   var transform = new Transform(operations, options);
 
   strictEqual(transform.id, options.id, 'id was populated');
-  strictEqual(transform.operations, operations, 'operations was populated');
+  equalOps(transform.operations, operations, 'operations was populated');
   strictEqual(transform.log, options.log, 'log was populated');
 });
-
