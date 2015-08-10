c62f543713096a2af99e8d1b9a463a2ef189e8b7
Dan Gebhardt <dan@cerebris.com>
2015-08-10 08:22:18 -0400

    Introduce Source#prepareTransformOperations.
    Move logic that coalesces and evaluates the necessity of transform
    operations from TransformConnector to Source.

--- test/tests/orbit-common/unit/source-test.js
+++ test/tests/orbit-common/unit/source-test.js
@@ -1,9 +1,11 @@
 import Orbit from 'orbit/main';
 import Schema from 'orbit-common/schema';
 import Source from 'orbit-common/source';
+import Operation from 'orbit/operation';
 import { all, Promise } from 'rsvp';
 import CacheIntegrityProcessor from 'orbit-common/operation-processors/cache-integrity-processor';
 import SchemaConsistencyProcessor from 'orbit-common/operation-processors/schema-consistency-processor';
+import { equalOps } from 'tests/test-helper';
 
 var schema;
 
@@ -44,4 +46,47 @@ test("can be created with a cache with `useCache`, and options can be specified 
   var source = new Source(schema, {useCache: true, cacheOptions: {processors: [CacheIntegrityProcessor, SchemaConsistencyProcessor]}});
   ok(source._cache, 'cache exists');
   equal(source._cache._processors.length, 2, 'cache has 2 processors');
+});
+
+test("#_prepareTransformOperations - for `add` operations, applies a differential if the target path exists", function() {
+  expect(1);
+
+  var source = new Source(schema, {useCache: true, cacheOptions: {processors: [CacheIntegrityProcessor, SchemaConsistencyProcessor]}});
+
+  var op = new Operation({
+    op: 'add',
+    path: ['planet', '1'],
+    value: {id: 1, name: 'Earth', hasRings: false}
+  });
+
+  source.retrieve = function() {
+    return {
+      id: 1,
+      name: 'Saturn',
+      hasRings: true
+    };
+  };
+
+  var result = source._prepareTransformOperations([op]);
+  equalOps(result, [{op: 'replace', path: 'planet/1/name', value: 'Earth'},
+                    {op: 'replace', path: 'planet/1/hasRings', value: false}]);
+});
+
+test("#_prepareTransformOperations - for `replace` operations, applies a differential if the target path exists", function() {
+  expect(1);
+
+  var source = new Source(schema, {useCache: true, cacheOptions: {processors: [CacheIntegrityProcessor, SchemaConsistencyProcessor]}});
+
+  var op = new Operation({
+    op: 'replace',
+    path: ['planet', '1', 'hasRings'],
+    value: true
+  });
+
+  source.retrieve = function() {
+    return false;
+  };
+
+  var result = source._prepareTransformOperations([op]);
+  equalOps(result, [{op: 'replace', path: 'planet/1/hasRings', value: true}]);
 });
