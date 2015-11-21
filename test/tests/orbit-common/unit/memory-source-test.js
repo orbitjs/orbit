08f60810142cdc068b75b711234766215bbcd4c8
opsb <oliver@opsb.co.uk>
2015-11-21 17:19:53 +0100

    Update to new qunit conventions

--- test/tests/orbit-common/unit/memory-source-test.js
+++ test/tests/orbit-common/unit/memory-source-test.js
@@ -12,12 +12,12 @@ import {
   queryExpression as oqe
 } from 'orbit-common/oql/expressions';
 
-var schema, source;
-
 ///////////////////////////////////////////////////////////////////////////////
 
-module("OC - MemorySource", {
-  setup: function() {
+module("OC - MemorySource", function(hooks) {
+  let schema, source;
+
+  hooks.beforeEach(function() {
     Orbit.Promise = Promise;
 
     schema = new Schema({
@@ -43,70 +43,72 @@ module("OC - MemorySource", {
     });
 
     source = new MemorySource({schema: schema});
-  },
+  });
 
-  teardown: function() {
+  hooks.afterEach(function() {
     schema = null;
     source = null;
     Orbit.Promise = null;
-  }
-});
-
-test("it exists", function(assert) {
-  assert.ok(source);
-});
-
-test("its prototype chain is correct", function(assert) {
-  assert.ok(source instanceof Source, 'instanceof Source');
-});
-
-test("#transform - transforms the source's cache", function(assert) {
-  expect(3);
-
-  let jupiter = {
-    id: '1',
-    type: 'planet',
-    attributes: { name: 'Jupiter', classification: 'gas giant' }
-  };
-
-  let op = {
-    op: 'add',
-    path: 'planet/1',
-    value: jupiter
-  };
-
-  assert.equal(source.cache.length('planet'), 0, 'cache should start empty');
-
-  stop();
-  source.transform(op)
-    .then(function(result) {
-      start();
-      assert.equal(source.cache.length('planet'), 1, 'cache should contain one planet');
-      assert.deepEqual(source.cache.get('planet/1'), jupiter, 'planet should be jupiter');
-    });
-});
-
-test("#query - queries the source's cache", function(assert) {
-  expect(2);
-
-  let jupiter = {
-    id: '1',
-    type: 'planet',
-    attributes: { name: 'Jupiter', classification: 'gas giant' }
-  };
-
-  source.cache.reset({
-    planet: {
-      '1': jupiter
-    }
   });
 
-  assert.equal(source.cache.length('planet'), 1, 'cache should contain one planet');
+  test("it exists", function(assert) {
+    assert.ok(source);
+  });
 
-  stop();
-  source.query({ oql: oqe('get', 'planet/1') })
-    .then(function(foundPlanet) {
-      start();
-      strictEqual(foundPlanet, jupiter, 'found planet matches original');
+  test("its prototype chain is correct", function(assert) {
+    assert.ok(source instanceof Source, 'instanceof Source');
+  });
+
+  test("#transform - transforms the source's cache", function(assert) {
+    const done = assert.async();
+    expect(3);
+
+    let jupiter = {
+      id: '1',
+      type: 'planet',
+      attributes: { name: 'Jupiter', classification: 'gas giant' }
+    };
+
+    let op = {
+      op: 'add',
+      path: 'planet/1',
+      value: jupiter
+    };
+
+    assert.equal(source.cache.length('planet'), 0, 'cache should start empty');
+
+    source.transform(op)
+      .then(function(result) {
+        assert.equal(source.cache.length('planet'), 1, 'cache should contain one planet');
+        assert.deepEqual(source.cache.get('planet/1'), jupiter, 'planet should be jupiter');
+        done();
+      });
+  });
+
+  test("#query - queries the source's cache", function(assert) {
+    const done = assert.async();
+    expect(2);
+
+    let jupiter = {
+      id: '1',
+      type: 'planet',
+      attributes: { name: 'Jupiter', classification: 'gas giant' }
+    };
+
+    source.cache.reset({
+      planet: {
+        '1': jupiter
+      }
     });
+
+    assert.equal(source.cache.length('planet'), 1, 'cache should contain one planet');
+
+    source.query({ oql: oqe('get', 'planet/1') })
+      .then(function(foundPlanet) {
+        strictEqual(foundPlanet, jupiter, 'found planet matches original');
+        done();
+      });
+  });
 });
+
+
