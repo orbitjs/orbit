1089d9d675ec48d115c98828e4302b4de629bfc1
opsb <oliver@opsb.co.uk>
2015-12-22 23:20:21 -0700

    wip

--- test/tests/orbit-common/unit/cache/subscriptions-test.js
+++ test/tests/orbit-common/unit/cache/subscriptions-test.js
@@ -13,39 +13,59 @@ import {
   queryExpression as oqe
 } from 'orbit-common/oql/expressions';
 
+const { skip } = QUnit;
 
 module('OC - Cache - subscriptions', function(hooks) {
   let cache;
+  let pluto;
+  let jupiter;
+  let callisto;
+  let saturn;
+  let titan;
 
   hooks.beforeEach(function() {
+    pluto = planetsSchema.normalize({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
+
+    jupiter = planetsSchema.normalize({ type: 'planet', id: 'jupiter', attributes: { name: 'Jupiter' } });
+    callisto = planetsSchema.normalize({ type: 'moon', id: 'callisto', attributes: { name: 'Callisto' } });
+
+    saturn = planetsSchema.normalize({
+      type: 'planet', id: 'saturn',
+      attributes: { name: 'Saturn' },
+      relationships: { moons: { data: { 'moon:titan': true } } } });
+
+    titan = planetsSchema.normalize({
+      type: 'moon', id: 'titan',
+      attributes: { name: 'Titan' },
+      relationships: { planet: { data: 'planet:saturn' } } });
+
     cache = new Cache(planetsSchema);
   });
 
   test('recordsOfType', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const addJupiter = addRecordOperation({ type: 'planet', id: 'jupiter', attributes: { name: 'Jupiter' } });
-    const addCallisto = addRecordOperation({ type: 'moon', id: 'callisto', attributes: { name: 'Callisto' } });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(addJupiter));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(addRecordOperation(jupiter)));
     cache.patches.onCompleted();
 
     const subscription = cache.subscribe({ oql: oqe('recordsOfType', 'planet') });
 
     subscription.toArray().subscribe((operations) => {
-      equalOps(operations, [addPluto, addJupiter]);
+      equalOps(operations, [
+        addRecordOperation(pluto),
+        addRecordOperation(jupiter)
+      ]);
+
       done();
     });
   });
 
   test('filter - add new match', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const addJupiter = addRecordOperation({ type: 'planet', id: 'jupiter', attributes: { name: 'Jupiter' } });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(addJupiter));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(addRecordOperation(jupiter)));
     cache.patches.onCompleted();
 
     const subscription = cache.subscribe({
@@ -55,15 +75,13 @@ module('OC - Cache - subscriptions', function(hooks) {
           oqe('equal', oqe('get', 'attributes/name'), 'Pluto')) });
 
     subscription.toArray().subscribe((operations) => {
-      equalOps(operations, [addPluto]);
+      equalOps(operations, [addRecordOperation(pluto)]);
       done();
     });
   });
 
   test('filter - remove match', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const removePluto = removeRecordOperation({ type: 'planet', id: 'pluto' });
 
     const subscription = cache.subscribe({
       oql:
@@ -72,19 +90,20 @@ module('OC - Cache - subscriptions', function(hooks) {
           oqe('equal', oqe('get', 'attributes/name'), 'Pluto')) });
 
     subscription.toArray().subscribe(operations => {
-      equalOps(operations, [addPluto, removePluto]);
+      equalOps(operations, [
+        addRecordOperation(pluto),
+        removeRecordOperation(pluto)
+      ]);
       done();
     });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(removePluto));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(removeRecordOperation(pluto)));
     cache.patches.onCompleted();
   });
 
   test('filter - remove then add match', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const removePluto = removeRecordOperation({ type: 'planet', id: 'pluto' });
 
     const subscription = cache.subscribe({
       oql:
@@ -92,20 +111,18 @@ module('OC - Cache - subscriptions', function(hooks) {
           oqe('recordsOfType', 'planet'),
           oqe('equal', oqe('get', 'attributes/name'), 'Pluto')) });
 
-    cache.transform(new Transform(removePluto));
-    cache.transform(new Transform(addPluto));
+    cache.transform(new Transform(removeRecordOperation(pluto)));
+    cache.transform(new Transform(addRecordOperation(pluto)));
     cache.patches.onCompleted();
 
     subscription.take(2).toArray().subscribe((operations) => {
-      equalOps(operations, [addPluto]);
+      equalOps(operations, [addRecordOperation(pluto)]);
       done();
     });
   });
 
   test('filter - change attribute that causes removal from matches', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const renamePluto = replaceAttributeOperation({ type: 'planet', id: 'pluto' }, 'name', 'Jupiter');
 
     const subscription = cache.subscribe({
       oql:
@@ -118,36 +135,32 @@ module('OC - Cache - subscriptions', function(hooks) {
       done();
     });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(renamePluto));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(replaceAttributeOperation({ type: 'planet', id: 'pluto' }, 'name', 'Jupiter')));
     cache.patches.onCompleted();
   });
 
   test('filter - change attribute that causes add to matches', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const renamePluto = replaceAttributeOperation({ type: 'planet', id: 'pluto' }, 'name', 'Jupiter');
 
     const subscription = cache.subscribe({
       oql:
         oqe('filter',
           oqe('recordsOfType', 'planet'),
-          oqe('equal', oqe('get', 'attributes/name'), 'Jupiter')) });
+          oqe('equal', oqe('get', 'attributes/name'), 'Uranus2')) });
 
     subscription.subscribe(operation => {
-      equalOps(operation, addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Jupiter' } }));
+      equalOps(operation, addRecordOperation({ type: 'planet', id: 'uranus', attributes: { name: 'Uranus2' } }));
       done();
     });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(renamePluto));
+    cache.transform(new Transform(addRecordOperation({ type: 'planet', id: 'uranus', attributes: { name: 'Uranus' } })));
+    cache.transform(new Transform(replaceAttributeOperation({ type: 'planet', id: 'uranus' }, 'name', 'Uranus2')));
     cache.patches.onCompleted();
   });
 
   test('filter - ignores remove record that isn\'t included in matches', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const removePluto = removeRecordOperation({ type: 'planet', id: 'pluto' });
 
     const subscription = cache.subscribe({
       oql:
@@ -160,81 +173,210 @@ module('OC - Cache - subscriptions', function(hooks) {
       done();
     });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(removePluto));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(removeRecordOperation(pluto)));
     cache.patches.onCompleted();
   });
 
   test('record - responds to record added/removed', function(assert) {
     const done = assert.async();
-    const addPluto = addRecordOperation({ type: 'planet', id: 'pluto', attributes: { name: 'Pluto' } });
-    const removePluto = removeRecordOperation({ type: 'planet', id: 'pluto' });
 
     const subscription = cache.subscribe({ oql: oqe('record', 'planet', 'pluto') });
 
     subscription.toArray().subscribe(operations => {
-      equalOps(operations, [addPluto, removePluto]);
+      equalOps(operations, [
+        addRecordOperation(pluto),
+        removeRecordOperation(pluto)
+      ]);
+
       done();
     });
 
-    cache.transform(new Transform(addPluto));
-    cache.transform(new Transform(removePluto));
+    cache.transform(new Transform(addRecordOperation(pluto)));
+    cache.transform(new Transform(removeRecordOperation(pluto)));
     cache.patches.onCompleted();
   });
 
-  test('relatedRecord - responds to replace hasOne', function(assert) {
-    const done = assert.async();
-    const addJupiter = addRecordOperation({ type: 'planet', id: 'jupiter', attributes: { name: 'Jupiter' } });
-    const addCallisto = addRecordOperation({ type: 'planet', id: 'callisto', attributes: { name: 'Callisto' } });
-    const addJupiterToCallisto = replaceHasOneOperation({ type: 'moon', id: 'callisto' }, 'planet', { type: 'planet', id: 'jupiter' });
-    const removeJupiterFromCallisto = replaceHasOneOperation({ type: 'moon', id: 'callisto' }, 'planet', null);
+  module('relatedRecord', function() {
+    test('adds and removes record from subscription', function(assert) {
+      const done = assert.async();
 
-    const query = {
-      oql: oqe('relatedRecord', 'moon', 'callisto', 'planet') };
+      cache.reset({ planet: { jupiter }, moon: { callisto } });
 
-    const eventRecorder = new EventRecorder('recordAdded', 'recordRemoved');
-    const subscription = cache.subscribe(query, { listeners: eventRecorder });
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecord', 'moon', 'callisto', 'planet') });
 
-    eventRecorder.take(2).then(events => {
-      deepEqual(events, [
-        { type: 'recordAdded', value: 'jupiter' },
-        { type: 'recordRemoved', value: 'jupiter' }
-      ]);
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          addRecordOperation(jupiter),
+          removeRecordOperation(jupiter)
+        ]);
 
-      done();
+        done();
+      });
+
+      cache.transform(new Transform(replaceHasOneOperation(callisto, 'planet', jupiter)));
+      cache.transform(new Transform(replaceHasOneOperation(callisto, 'planet', null)));
+      cache.patches.onCompleted();
     });
 
-    cache.transform(new Transform(addJupiter));
-    cache.transform(new Transform(addCallisto));
-    cache.transform(new Transform(addJupiterToCallisto));
-    cache.transform(new Transform(removeJupiterFromCallisto));
+    test('emits updates for record after it\'s added to subscription', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { jupiter }, moon: { callisto } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecord', 'moon', 'callisto', 'planet') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          addRecordOperation(jupiter),
+          replaceAttributeOperation(jupiter, 'name', 'Jupiter2')
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(replaceHasOneOperation(callisto, 'planet', jupiter)));
+      cache.transform(new Transform(replaceAttributeOperation(jupiter, 'name', 'Jupiter2')));
+      cache.patches.onCompleted();
+    });
+
+    test('emits updates for initial record', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { saturn }, moon: { titan } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecord', 'moon', 'titan', 'planet') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          replaceAttributeOperation(saturn, 'name', 'Saturn2')
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(replaceAttributeOperation(saturn, 'name', 'Saturn2')));
+      cache.patches.onCompleted();
+    });
+
+    test('stops emitting updates for related record once it\'s been removed', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { saturn }, moon: { titan } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecord', 'moon', 'titan', 'planet') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          removeRecordOperation(saturn)
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(replaceHasOneOperation(titan, 'planet', null)));
+      cache.transform(new Transform(replaceAttributeOperation(saturn, 'name', 'Saturn2')));
+      cache.patches.onCompleted();
+    });
   });
 
-  test('relatedRecords - responds to add record', function(assert) {
-    const done = assert.async();
-    const addJupiter = addRecordOperation({ type: 'planet', id: 'jupiter', attributes: { name: 'Jupiter' } });
-    const addCallisto = addRecordOperation({ type: 'planet', id: 'callisto', attributes: { name: 'Callisto' } });
-    const addCallistoToJupiter = addToHasManyOperation({ type: 'planet', id: 'jupiter' }, 'moons', { type: 'moon', id: 'callisto' });
-    const removeCallistoFromJupiter = removeFromHasManyOperation({ type: 'planet', id: 'jupiter' }, 'moons', { type: 'moon', id: 'callisto' });
+  module('relatedRecords', function() {
+    test('adds and removes records from subscription', function(assert) {
+      const done = assert.async();
 
-    const query = { oql: oqe('relatedRecords', 'planet', 'jupiter', 'moons') };
+      cache.reset({ planet: { jupiter }, moon: { callisto } });
 
-    const eventRecorder = new EventRecorder('recordAdded', 'recordRemoved');
-    const subscription = cache.subscribe(query, { listeners: eventRecorder });
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecords', 'planet', 'jupiter', 'moons') });
 
-    eventRecorder.take(2).then(events => {
-      deepEqual(events, [
-        { type: 'recordAdded', value: 'callisto' },
-        { type: 'recordRemoved', value: 'callisto' }
-      ]);
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          addRecordOperation(callisto),
+          removeRecordOperation(callisto)
+        ]);
 
-      done();
+        done();
+      });
+
+      cache.transform(new Transform(addToHasManyOperation(jupiter, 'moons', callisto)));
+      cache.transform(new Transform(removeFromHasManyOperation(jupiter, 'moons', callisto)));
+      cache.patches.onCompleted();
     });
 
-    cache.transform(new Transform(addJupiter));
-    cache.transform(new Transform(addCallisto));
-    cache.transform(new Transform(addCallistoToJupiter));
-    cache.transform(new Transform(removeCallistoFromJupiter));
+    test('emits updates to records included in subscription', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { jupiter }, moon: { callisto } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecords', 'planet', 'jupiter', 'moons') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          addRecordOperation(callisto),
+          replaceAttributeOperation(callisto, 'name', 'Callisto2')
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(addToHasManyOperation(jupiter, 'moons', callisto)));
+      cache.transform(new Transform(replaceAttributeOperation(callisto, 'name', 'Callisto2')));
+      cache.patches.onCompleted();
+    });
+
+    test('emits updates to records included in subscription with initial values', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { saturn }, moon: { titan } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecords', 'planet', 'saturn', 'moons') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          replaceAttributeOperation(titan, 'name', 'Titan2')
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(replaceAttributeOperation(titan, 'name', 'Titan2')));
+      cache.patches.onCompleted();
+    });
+
+    test('stops emits updates to records that have been removed from subscription', function(assert) {
+      const done = assert.async();
+
+      cache.reset({ planet: { saturn }, moon: { titan } });
+
+      const subscription = cache.subscribe({
+        oql:
+          oqe('relatedRecords', 'planet', 'saturn', 'moons') });
+
+      subscription.toArray().subscribe(operations => {
+        equalOps(operations, [
+          removeRecordOperation(titan)
+        ]);
+
+        done();
+      });
+
+      cache.transform(new Transform(removeFromHasManyOperation(saturn, 'moons', titan)));
+      cache.transform(new Transform(replaceAttributeOperation(titan, 'name', 'Titan2')));
+      cache.patches.onCompleted();
+    });
   });
 });
 
