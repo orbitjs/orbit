6585492a6dbff63aff94adab61d4f88bd7ba6a87
Dan Gebhardt <dan@cerebris.com>
2016-02-26 08:56:05 -0500

    Modernize transformable-test.js.

--- test/tests/orbit/unit/transformable-test.js
+++ test/tests/orbit/unit/transformable-test.js
@@ -9,97 +9,91 @@ let Source, source;
 ///////////////////////////////////////////////////////////////////////////////
 
 module('Orbit - Transformable', {
-  setup: function() {
+  setup() {
     Source = Class.extend(Transformable);
     source = new Source();
   },
 
-  teardown: function() {
+  teardown() {
     Source = source = null;
   }
 });
 
-test('it exists', function() {
-  ok(source);
+test('it exists', function(assert) {
+  assert.ok(source);
 });
 
-test('it should mixin Evented', function() {
+test('it should mixin Evented', function(assert) {
   ['on', 'off', 'emit', 'poll'].forEach(function(prop) {
-    ok(source[prop], 'should have Evented properties');
+    assert.ok(source[prop], 'should have Evented properties');
   });
 });
 
-test('it defines `transform`', function() {
-  ok(source.transform, 'transform exists');
+test('it defines `transform`', function(assert) {
+  assert.ok(source.transform, 'transform exists');
 });
 
-test('it should require the definition of _transform', function() {
-  throws(source._transform, 'presence of _transform should be verified');
+test('it should require the definition of _transform', function(assert) {
+  assert.throws(source._transform, 'presence of _transform should be verified');
 });
 
 
-test('it should resolve when _transform returns a promise', function() {
-  expect(2);
+test('it should resolve when _transform returns a promise', function(assert) {
+  assert.expect(2);
 
   source._transform = function(o) {
     return new Promise(function(resolve, reject) {
-      ok(true, '_transform promise resolved');
+      assert.ok(true, '_transform promise resolved');
       resolve(':)');
     });
   };
 
-  stop();
-  source.transform({ op: 'add', path: 'planet/1', value: 'data' })
+  return source.transform({ op: 'add', path: 'planet/1', value: 'data' })
     .then(() => {
-      start();
-      ok(true, 'transform promise resolved');
+      assert.ok(true, 'transform promise resolved');
     });
 });
 
-test('it should resolve when _transform simply returns (without a promise)', function() {
-  expect(2);
+test('it should resolve when _transform simply returns (without a promise)', function(assert) {
+  assert.expect(2);
 
   source._transform = function() {
-    ok(true, '_transform called');
+    assert.ok(true, '_transform called');
     return ':)';
   };
 
-  stop();
-  source.transform({ op: 'add', path: 'planet/1', value: 'data' })
+  return source.transform({ op: 'add', path: 'planet/1', value: 'data' })
     .then(() => {
-      start();
-      ok(true, 'transform promise returned');
+      assert.ok(true, 'transform promise returned');
     });
 });
 
-test('it should trigger `transform` event BEFORE a transform resolves', function() {
-  expect(5);
+test('it should trigger `transform` event BEFORE a transform resolves', function(assert) {
+  assert.expect(5);
 
   let order = 0;
   let addOps = [{ op: 'add', path: 'planet/1', value: 'data' }];
   let inverseOps = [{ op: 'remove', path: 'planet/1' }];
 
   source._transform = function(transform) {
-    equal(++order, 1, '_transform performed first');
+    assert.equal(++order, 1, '_transform performed first');
     equalOps(transform.operations, addOps, '_handler args match original call args');
     this.transformed(transform);
   };
 
   source.on('transform', function(transform) {
-    equal(++order, 2, 'transform triggered after action performed successfully');
+    assert.equal(++order, 2, 'transform triggered after action performed successfully');
     equalOps(transform.operations, addOps, 'applied ops match');
   });
 
-  stop();
-  source.transform(addOps)
+  return source.transform(addOps)
     .then(() => {
-      start();
-      equal(++order, 3, 'promise resolved last');
+      assert.equal(++order, 3, 'promise resolved last');
     });
 });
 
-test('it should perform transforms in the order they are pushed', function() {
-  expect(4);
+test('it should perform transforms in the order they are pushed', function(assert) {
+  assert.expect(4);
 
   let order = 0;
   let addOp = { op: 'add', path: 'planet/1', value: 'data' };
@@ -107,23 +101,21 @@ test('it should perform transforms in the order they are pushed', function() {
 
   source._transform = function(transform) {
     source.settleTransforms().then(function() {
-      start();
-      equal(++order, 3, 'settleTransforms finishes after all other transforms');
+      assert.equal(++order, 3, 'settleTransforms finishes after all other transforms');
     });
 
     equalOps(transform.operations, [addOp, inverseOp]);
-    equal(++order, 1, '_transform called first');
+    assert.equal(++order, 1, '_transform called first');
   };
 
-  stop();
-  source.transform([addOp, inverseOp])
+  return source.transform([addOp, inverseOp])
     .then(() => {
-      equal(++order, 2, 'promise resolved last');
+      assert.equal(++order, 2, 'promise resolved last');
     });
 });
 
-test('it should wait for the current settle loop before starting another', function() {
-  expect(8);
+test('it should wait for the current settle loop before starting another', function(assert) {
+  assert.expect(8);
 
   let order = 0;
   let addOps = [{ op: 'add', path: 'planet/1', value: 'data' }];
@@ -135,15 +127,14 @@ test('it should wait for the current settle loop before starting another', funct
     // console.log('_transform', operation.serialize());
     if (transform.operations[0].op === 'add') {
       source.settleTransforms().then(function() {
-        start();
-        equal(++order, 6, 'settleTransforms finishes after all other transforms');
+        assert.equal(++order, 6, 'settleTransforms finishes after all other transforms');
       });
 
-      equal(++order, 1, '_transform `add` performed first');
+      assert.equal(++order, 1, '_transform `add` performed first');
     }
 
     if (transform.operations[0].op === 'remove') {
-      equal(++order, 3, '_transform `remove` performed second');
+      assert.equal(++order, 3, '_transform `remove` performed second');
     }
 
     this.transformed(transform);
@@ -151,38 +142,34 @@ test('it should wait for the current settle loop before starting another', funct
 
   source.on('transform', function(transform) {
     if (transform.operations[0].op === 'add') {
-      equal(++order, 2, 'didTransform triggered after `add` transform');
+      assert.equal(++order, 2, 'didTransform triggered after `add` transform');
       equalOps(transform.operations, addOps, '`add` operation matches');
     }
     if (transform.operations[0].op === 'remove') {
-      equal(++order, 4, 'didTransform triggered after `remove` transform');
+      assert.equal(++order, 4, 'didTransform triggered after `remove` transform');
       equalOps(transform.operations, inverseOps, '`remove` operation matches');
     }
   });
 
-  stop();
-
   source.transform(addOps);
-  source.transform(inverseOps)
+  return source.transform(inverseOps)
     .then(() => {
       equal(++order, 5, 'promise resolved last');
     });
 });
 
-test('#clearTransformLog can clear the log of any applied transforms', function() {
-  expect(2);
+test('#clearTransformLog can clear the log of any applied transforms', function(assert) {
+  assert.expect(2);
 
   source._transform = function(transform) {
     return this.transformed(transform);
   };
 
-  stop();
-  source.transform({ op: 'add', path: 'planet/1', value: 'data' })
+  return source.transform({ op: 'add', path: 'planet/1', value: 'data' })
     .then(() => {
-      start();
-      equal(Object.keys(source._transformLog).length, 1, 'log has an entry');
+      assert.equal(Object.keys(source._transformLog).length, 1, 'log has an entry');
 
       source.clearTransformLog();
-      equal(Object.keys(source._transformLog).length, 0, 'log has been cleared');
+      assert.equal(Object.keys(source._transformLog).length, 0, 'log has been cleared');
     });
 });
