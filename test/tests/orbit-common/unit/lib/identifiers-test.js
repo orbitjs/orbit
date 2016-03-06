2afff44cd0872ebc832676be6293e32b833a0d44
Dan Gebhardt <dan@cerebris.com>
2016-03-06 03:31:09 -0500

    Introduce `identity` helper.
    `identity` returns a simple { type, id } identity object from any
    object with a `type` and `id`.

--- test/tests/orbit-common/unit/lib/identifiers-test.js
+++ test/tests/orbit-common/unit/lib/identifiers-test.js
@@ -1,12 +1,16 @@
-import { toIdentifier, parseIdentifier } from 'orbit-common/lib/identifiers';
+import { toIdentifier, parseIdentifier, identity } from 'orbit-common/lib/identifiers';
 
 module('OC - Lib - Identifiers', {
 });
 
-test('`toIdentifier` converts a `type` and `id` to an identifier string', function() {
-  equal(toIdentifier('planet', '1'), 'planet:1');
+test('`toIdentifier` converts a `type` and `id` to an identifier string', function(assert) {
+  assert.equal(toIdentifier('planet', '1'), 'planet:1');
 });
 
-test('`parseIdentifier` converts an identifier string to an object with a `type` and `id`', function() {
-  deepEqual(parseIdentifier('planet:1'), { type: 'planet', id: '1' });
+test('`parseIdentifier` converts an identifier string to an object with a `type` and `id`', function(assert) {
+  assert.deepEqual(parseIdentifier('planet:1'), { type: 'planet', id: '1' });
+});
+
+test('`identity` returns a simple { type, id } identity object from any object with a `type` and `id`', function(assert) {
+  assert.deepEqual(identity({ type: 'planet', id: '1', attributes: { }, relationships: { } }), { type: 'planet', id: '1' });
 });
