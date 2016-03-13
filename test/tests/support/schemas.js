607035559c34569df4a45d0c7893f8d013637277
opsb <oliver@opsb.co.uk>
2016-03-13 14:53:49 +0100

    Update Store to use 'confirmation' workflow and builders

--- test/tests/support/schemas.js
+++ test/tests/support/schemas.js
@@ -5,15 +5,23 @@ var planetsSchema = new Schema({
   models: {
     planet: {
       keys: {
-        id: { primaryKey: true, defaultValue: uuid }
+        id: { primaryKey: true, defaultValue: uuid },
+        remoteId: { defaultValue: null }
+      },
+      attributes: {
+        name: { type: 'string' },
+        classification: { type: 'string' }
       },
       relationships: {
-        moons: { type: 'hasMany', model: 'moon' }
+        moons: { type: 'hasMany', model: 'moon', inverse: 'planet' }
       }
     },
     moon: {
+      attributes: {
+        name: { type: 'string' }
+      },
       relationships: {
-        planet: { type: 'hasOne', model: 'planet' }
+        planet: { type: 'hasOne', model: 'planet', inverse: 'moons' }
       }
     }
   }
