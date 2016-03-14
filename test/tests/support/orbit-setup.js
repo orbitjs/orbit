4998baaef204cb5c0692111be4f9731a78161e86
Dan Gebhardt <dan@cerebris.com>
2016-03-14 16:22:48 -0400

    Add Orbit.pluralize and Orbit.singularize methods for testing.

--- test/tests/support/orbit-setup.js
+++ test/tests/support/orbit-setup.js
@@ -4,3 +4,12 @@ import jQuery from 'jquery';
 
 Orbit.Promise = Promise;
 Orbit.ajax = jQuery.ajax;
+
+Orbit.pluralize = function(original) {
+  return original.match(/s$/) ? original : original + 's';
+};
+
+Orbit.singularize = function(original) {
+  const match = original.match(/(.*)s$/);
+  return match ? match[1] : original;
+};
