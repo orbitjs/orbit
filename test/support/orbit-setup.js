88d035aa69b6cdaa9190a1b7174155cc89cc250c
Dan Gebhardt <dan@cerebris.com>
2017-01-25 22:10:10 -0500

    Remove unnecessary setup of pluralize and singularize.

--- test/support/orbit-setup.js
+++ test/support/orbit-setup.js
@@ -2,12 +2,3 @@ import Orbit from '../../src';
 import { Promise } from 'rsvp';
 
 Orbit.Promise = Promise;
-
-Orbit.pluralize = function(original) {
-  return original.match(/s$/) ? original : original + 's';
-};
-
-Orbit.singularize = function(original) {
-  const match = original.match(/(.*)s$/);
-  return match ? match[1] : original;
-};
