afdcc90378808d1d3ab24584b92ddfffe1696bec
Dan Gebhardt <dan@cerebris.com>
2017-02-07 17:04:04 -0500

    Convert lib/deprecate to TS

--- src/lib/deprecate.ts
+++ src/lib/deprecate.ts
@@ -1,14 +1,11 @@
-/* eslint-disable valid-jsdoc */
-
 /**
  Display a deprecation warning with the provided message.
 
  @method deprecate
- @for Orbit
  @param {String} message Description of the deprecation
  @param {Boolean} test An optional boolean. If false, the deprecation will be displayed.
  */
-export function deprecate(message, test) {
+export function deprecate(message: string, test: () => boolean | boolean) {
   if (typeof test === 'function') {
     if (test()) { return; }
   } else {
