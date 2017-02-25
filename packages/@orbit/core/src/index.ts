d07c5737aebc3f196cb74c3496335730c64f0592
Dan Gebhardt <dan@cerebris.com>
2017-02-25 17:13:22 -0500

    Export source decorators

--- packages/@orbit/core/src/index.ts
+++ packages/@orbit/core/src/index.ts
@@ -18,3 +18,8 @@ export { default as Schema, AttributeDefinition, RelationshipDefinition, KeyDefi
 export { default as Source } from './source';
 export { default as TransformLog } from './transform-log';
 export { default as Transform } from './transform';
+export { default as pullable, Pullable, isPullable } from './source-decorators/pullable';
+export { default as pushable, Pushable, isPushable } from './source-decorators/pushable';
+export { default as queryable, Queryable, isQueryable } from './source-decorators/queryable';
+export { default as syncable, Syncable, isSyncable } from './source-decorators/syncable';
+export { default as updatable, Updatable, isUpdatable } from './source-decorators/updatable';
