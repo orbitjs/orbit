3da48d464b6e683e14d0c5bf5dc5e2d598cd2ddd
Dan Gebhardt <dan@cerebris.com>
2017-04-05 18:19:02 -0400

    Export default sources from other source packages.

--- packages/@orbit/jsonapi/src/index.ts
+++ packages/@orbit/jsonapi/src/index.ts
@@ -1,3 +1,3 @@
-export { default as JSONAPISource } from './jsonapi-source';
+export { default } from './jsonapi-source';
 export { default as JSONAPISerializer } from './jsonapi-serializer';
 export * from './jsonapi-document';
--- packages/@orbit/store/src/index.ts
+++ packages/@orbit/store/src/index.ts
@@ -1,3 +1,3 @@
-export { default as Store, StoreSettings, StoreMergeOptions } from './store';
+export { default, StoreSettings, StoreMergeOptions } from './store';
 export { default as Cache, CacheSettings } from './cache';
 export { default as ImmutableMap } from './immutable-map';
