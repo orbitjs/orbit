73acc668e2ee08ae7b1297c3baaa0848fb12a247
Dan Gebhardt <dan@cerebris.com>
2017-04-06 10:48:14 -0400

    Remove unused test matchers

--- packages/@orbit/core/test/support/qunit-matchers.ts
+++
@@ -1,6 +0,0 @@
-import { buildPatternMatcher } from './pattern-matcher';
-
-QUnit.assert.matchesPattern = function(actual, pattern, message) {
-  const isMatch = buildPatternMatcher(pattern);
-  this.push(isMatch(actual), actual, pattern, message);
-};
