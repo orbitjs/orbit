f5c9bb853cfa88a627add0f78293040f161d48ba
Dan Gebhardt <dan@cerebris.com>
2017-05-01 17:56:34 -0400

    Fix deactivation in LogTruncationStrategy

--- packages/@orbit/coordinator/src/strategies/log-truncation-strategy.ts
+++ packages/@orbit/coordinator/src/strategies/log-truncation-strategy.ts
@@ -6,15 +6,12 @@ import Orbit, {
 } from '@orbit/data';
 import { Dict, assert, objectValues } from '@orbit/utils';
 
-export interface LogTruncationOptions extends StrategyOptions {
-}
-
-export default class LogTruncationStrategy extends Strategy {
+export class LogTruncationStrategy extends Strategy {
   protected _reviewing: Promise<void>;
   protected _extraReviewNeeded: boolean;
   protected _transformListeners: Dict<(transform: Transform) => void>;
 
-  constructor(options: LogTruncationOptions = {}) {
+  constructor(options: StrategyOptions = {}) {
     options.name = options.name || 'log-truncation';
     super(options);
   }
@@ -34,6 +31,7 @@ export default class LogTruncationStrategy extends Strategy {
   deactivate(): Promise<any> {
     return super.deactivate()
       .then(() => {
+        this._sources.forEach(source => this._deactivateSource(source));
         this._transformListeners = null;
       });
   }
