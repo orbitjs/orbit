0ff9b2aa3e5fafe792d5f3b3f026151dc263311d
Dan Gebhardt <dan@cerebris.com>
2017-07-13 17:06:59 -0400

    Improve efficiency of KeyMap lookups
    Now uses separate maps for idsToKeys and keysToIds to make setting and
    getting one level less deep.

--- packages/@orbit/data/src/key-map.ts
+++ packages/@orbit/data/src/key-map.ts
@@ -3,51 +3,53 @@ import { Record } from './record';
 
 /**
  * Maintains a map between records' ids and keys.
- * 
+ *
  * @export
  * @class KeyMap
  */
 export default class KeyMap {
-  private _data: Dict<any>;
+  private _idsToKeys: Dict<Dict<string>>;
+  private _keysToIds: Dict<Dict<string>>;
 
   constructor() {
-    this._data = {};
+    this._idsToKeys = {};
+    this._keysToIds = {};
   }
 
   /**
    * Return a key value given a model type, key name, and id.
-   * 
-   * @param {string} type 
-   * @param {string} keyName 
-   * @param {string} idValue 
-   * @returns {string} 
-   * 
+   *
+   * @param {string} type
+   * @param {string} keyName
+   * @param {string} idValue
+   * @returns {string}
+   *
    * @memberOf KeyMap
    */
   idToKey(type: string, keyName: string, idValue: string): string {
-    return deepGet(this._data, [type, keyName, 'idToKeyMap', idValue]);
+    return deepGet(this._idsToKeys, [type, keyName, idValue]);
   }
 
   /**
    * Return an id value given a model type, key name, and key value.
-   * 
-   * @param {string} type 
-   * @param {string} keyName 
-   * @param {string} keyValue 
-   * @returns {string} 
-   * 
+   *
+   * @param {string} type
+   * @param {string} keyName
+   * @param {string} keyValue
+   * @returns {string}
+   *
    * @memberOf KeyMap
    */
   keyToId(type: string, keyName: string, keyValue: string): string {
-    return deepGet(this._data, [type, keyName, 'keyToIdMap', keyValue]);
+    return deepGet(this._keysToIds, [type, keyName, keyValue]);
   }
 
   /**
    * Store the id and key values of a record in this key map.
-   * 
-   * @param {Record} record 
-   * @returns {void} 
-   * 
+   *
+   * @param {Record} record
+   * @returns {void}
+   *
    * @memberOf KeyMap
    */
   pushRecord(record: Record): void {
@@ -59,18 +61,18 @@ export default class KeyMap {
 
     Object.keys(keys).forEach(keyName => {
       let keyValue = keys[keyName];
-      deepSet(this._data, [type, keyName, 'idToKeyMap', id], keyValue);
-      deepSet(this._data, [type, keyName, 'keyToIdMap', keyValue], id);
+      deepSet(this._idsToKeys, [type, keyName, id], keyValue);
+      deepSet(this._keysToIds, [type, keyName, keyValue], id);
     });
   }
 
   /**
    * Given a record, find the cached id if it exists.
-   * 
-   * @param {string} type 
-   * @param {Dict<string>} keys 
-   * @returns {string} 
-   * 
+   *
+   * @param {string} type
+   * @param {Dict<string>} keys
+   * @returns {string}
+   *
    * @memberOf KeyMap
    */
   idFromKeys(type: string, keys: Dict<string>): string {
