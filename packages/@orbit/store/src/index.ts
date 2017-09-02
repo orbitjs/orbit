9b9de7a380bd212edd8d196db749d2f848631b84
Dan Gebhardt <dan@cerebris.com>
2017-09-02 15:09:07 -0400

    Export operation processors from @orbit/store

--- packages/@orbit/store/src/index.ts
+++ packages/@orbit/store/src/index.ts
@@ -1,2 +1,6 @@
 export { default, StoreSettings, StoreMergeOptions } from './store';
 export { default as Cache, CacheSettings, PatchResult, PatchResultData } from './cache';
+export { OperationProcessorClass, OperationProcessor } from './cache/operation-processors/operation-processor';
+export { default as CacheIntegrityProcessor } from './cache/operation-processors/cache-integrity-processor';
+export { default as SchemaConsistencyProcessor } from './cache/operation-processors/schema-consistency-processor';
+export { default as SchemaValidationProcessor } from './cache/operation-processors/schema-validation-processor';
