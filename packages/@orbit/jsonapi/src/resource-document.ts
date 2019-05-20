3f71226529b436e5c683e592c8e22b246415781c
Paul Chavard <git@paul.chavard.net>
2019-05-21 00:24:43 +0200

    Add deserializeOperationsDocument to JSONAPISerializer

--- packages/@orbit/jsonapi/src/resource-document.ts
+++ packages/@orbit/jsonapi/src/resource-document.ts
@@ -32,11 +32,91 @@ export interface Resource {
   links?: Dict<Link>;
 }
 
+export interface ResourceOperation {
+  op: 'get' | 'add' | 'update' | 'remove';
+  ref: {
+    type: string;
+    id?: string;
+    relationship?: string;
+  };
+  data?: Resource | Resource[];
+}
+
+export interface AddResourceOperation extends ResourceOperation {
+  op: 'add';
+  ref: {
+    type: string;
+    id?: string;
+  };
+  data: Resource;
+}
+
+export interface UpdateResourceOperation extends ResourceOperation {
+  op: 'update';
+  ref: {
+    type: string;
+    id: string;
+  };
+  data: Resource;
+}
+
+export interface RemoveResourceOperation extends ResourceOperation {
+  op: 'remove';
+  ref: {
+    type: string;
+    id: string;
+  };
+}
+
+export interface AddToRelatedResourcesOperation extends ResourceOperation {
+  op: 'add';
+  ref: {
+    type: string;
+    id: string;
+    relationship: string;
+  };
+  data: Resource;
+}
+
+export interface RemoveFromRelatedResourcesOperation extends ResourceOperation {
+  op: 'remove';
+  ref: {
+    type: string;
+    id: string;
+    relationship: string;
+  };
+  data: Resource;
+}
+
+export interface ReplaceRelatedResourceOperation extends ResourceOperation {
+  op: 'update';
+  ref: {
+    type: string;
+    id: string;
+    relationship: string;
+  };
+  data: Resource;
+}
+
+export interface ReplaceRelatedResourcesOperation extends ResourceOperation {
+  op: 'update';
+  ref: {
+    type: string;
+    id: string;
+    relationship: string;
+  };
+  data: Resource[];
+}
+
 export interface ResourceDocument {
   data: Resource | Resource[] | ResourceIdentity | ResourceIdentity[];
   included?: Resource[];
   meta?: Dict<any>;
   links?: Dict<Link>;
+}
+
+export interface ResourceOperationsDocument {
+  operations: ResourceOperation[];
 }
 
 /**
