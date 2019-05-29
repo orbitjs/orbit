934be0be09685e60ca506b16a9531cc74a12e0e1
Paul Chavard <git@paul.chavard.net>
2019-05-29 21:17:21 +0200

    Add prettier to @orbit/local-storage

---
+++ packages/@orbit/local-storage/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    browser: true
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-member-accessibility': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-object-literal-type-assertion': ['off']
+  },
+  overrides: {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['off']
+    }
+  }
+};
