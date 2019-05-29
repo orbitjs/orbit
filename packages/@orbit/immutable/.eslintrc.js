0a6c253242eabdc85960f14810270806a345187a
Paul Chavard <git@paul.chavard.net>
2019-05-29 12:56:28 +0200

    Add prettier to @orbit/immutable

---
+++ packages/@orbit/immutable/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    browser: true
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-member-accessibility': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off']
+  },
+  overrides: {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['off']
+    }
+  }
+};
