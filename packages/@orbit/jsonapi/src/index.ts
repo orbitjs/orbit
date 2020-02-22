6d90bb3555ba39e37a7a2cc44abe3a3ce2c2a2c3
Paul Chavard <git@paul.chavard.net>
2020-02-22 17:27:08 +0100

    Export all the types from jsonapi source

--- packages/@orbit/jsonapi/src/index.ts
+++ packages/@orbit/jsonapi/src/index.ts
@@ -1,6 +1,13 @@
-export { default } from './jsonapi-source';
+export { default, JSONAPISourceSettings } from './jsonapi-source';
 export * from './jsonapi-serializer';
-export { default as JSONAPIRequestProcessor } from './jsonapi-request-processor';
-export { default as JSONAPIURLBuilder } from './jsonapi-url-builder';
+export {
+  default as JSONAPIRequestProcessor,
+  JSONAPIRequestProcessorSettings,
+  FetchSettings
+} from './jsonapi-request-processor';
+export {
+  default as JSONAPIURLBuilder,
+  JSONAPIURLBuilderSettings
+} from './jsonapi-url-builder';
 export * from './record-document';
 export * from './resource-document';
