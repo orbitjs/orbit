c9883abb1a13dff9d85773fbc0872dc65e1dc600
Dan Gebhardt <dan@cerebris.com>
2020-03-22 17:06:45 -0400

    Introduce requestOptionsForSource
    Merges source-specific options with general options

--- packages/@orbit/data/src/request.ts
+++ packages/@orbit/data/src/request.ts
@@ -4,3 +4,26 @@ export interface RequestOptions extends Options {
   sources?: { [name: string]: { [key: string]: unknown } };
 }
 
+/**
+ * Merges general request options with those specific to a source. The more
+ * specific options override the general options.
+ */
+export function requestOptionsForSource(
+  options: RequestOptions,
+  source: string
+): Options {
+  if (options.sources) {
+    let { sources, ...rest } = options;
+
+    if (sources && sources[source]) {
+      return {
+        ...rest,
+        ...sources[source]
+      };
+    } else {
+      return rest;
+    }
+  } else {
+    return options;
+  }
+}
