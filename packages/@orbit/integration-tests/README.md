8ffcb300fe37abd54616d5757580b6dbff8ecd35
Dan Gebhardt <dan@cerebris.com>
2020-05-09 13:53:24 -0400

    Reintroduce @orbit/integration-tests
    This private package was accidentally dropped during the build refactor.

---
+++ packages/@orbit/integration-tests/README.md
@@ -0,0 +1,61 @@
+# @orbit/integration-tests
+
+This package is used to perform integration tests for core Orbit packages. It's
+useful for simulating complex interactions among sources and strategies.
+
+This is a private package with no publishable source code.
+
+## Installation
+
+Install with yarn:
+
+```
+yarn add @orbit/jsonapi
+```
+
+Or with npm:
+
+```
+npm install @orbit/jsonapi
+```
+
+## Contributing
+
+### Installation
+
+To install dependencies:
+
+```
+yarn install
+```
+
+### Building
+
+Distributions can be built to the `/dist` directory by running:
+
+```
+yarn build
+```
+
+### Testing
+
+#### CI Testing
+
+Test in CI mode by running:
+
+```
+yarn test
+```
+
+#### Browser Testing
+
+Test within a browser
+(at [http://localhost:7357/](http://localhost:7357/)) by running:
+
+```
+yarn testem
+```
+
+## License
+
+Copyright 2014-2020 Cerebris Corporation. MIT License (see LICENSE for details).
---
+++ packages/@orbit/integration-tests/test/store-jsonapi-remoteid-pessimistic-test.ts
@@ -0,0 +1,169 @@
+import { KeyMap, Record, Schema } from '@orbit/data';
+import JSONAPISource from '@orbit/jsonapi';
+import MemorySource from '@orbit/memory';
+import Coordinator, { RequestStrategy, SyncStrategy } from '@orbit/coordinator';
+import { jsonapiResponse } from './support/jsonapi';
+import { SinonStub } from 'sinon';
+import * as sinon from 'sinon';
+
+const { module, test } = QUnit;
+
+module(
+  'Store + JSONAPISource + remote IDs + pessimistic coordination',
+  function (hooks) {
+    let fetchStub: SinonStub;
+    let keyMap: KeyMap;
+    let schema: Schema;
+    let remote: JSONAPISource;
+    let memory: MemorySource;
+    let coordinator: Coordinator;
+
+    hooks.beforeEach(() => {
+      fetchStub = sinon.stub(self, 'fetch');
+
+      schema = new Schema({
+        models: {
+          planet: {
+            keys: {
+              remoteId: {}
+            },
+            attributes: {
+              name: { type: 'string' },
+              classification: { type: 'string' },
+              lengthOfDay: { type: 'number' }
+            },
+            relationships: {
+              moons: { kind: 'hasMany', type: 'moon', inverse: 'planet' },
+              solarSystem: {
+                kind: 'hasOne',
+                type: 'solarSystem',
+                inverse: 'planets'
+              }
+            }
+          },
+          moon: {
+            keys: {
+              remoteId: {}
+            },
+            attributes: {
+              name: { type: 'string' }
+            },
+            relationships: {
+              planet: { kind: 'hasOne', type: 'planet', inverse: 'moons' }
+            }
+          },
+          solarSystem: {
+            keys: {
+              remoteId: {}
+            },
+            attributes: {
+              name: { type: 'string' }
+            },
+            relationships: {
+              planets: {
+                kind: 'hasMany',
+                type: 'planet',
+                inverse: 'solarSystem'
+              }
+            }
+          }
+        }
+      });
+
+      keyMap = new KeyMap();
+
+      memory = new MemorySource({ schema, keyMap });
+
+      remote = new JSONAPISource({ schema, keyMap, name: 'remote' });
+      remote.requestProcessor.serializer.resourceKey = function () {
+        return 'remoteId';
+      };
+
+      coordinator = new Coordinator({
+        sources: [memory, remote]
+      });
+
+      // Query the remote server whenever the memory source is queried
+      coordinator.addStrategy(
+        new RequestStrategy({
+          source: 'memory',
+          on: 'beforeQuery',
+          target: 'remote',
+          action: 'pull',
+          blocking: true
+        })
+      );
+
+      // Update the remote server whenever the memory source is updated
+      coordinator.addStrategy(
+        new RequestStrategy({
+          source: 'memory',
+          on: 'beforeUpdate',
+          target: 'remote',
+          action: 'update',
+          blocking: true,
+          passHints: true
+        })
+      );
+
+      // Sync all changes received from the remote server to the memory source
+      coordinator.addStrategy(
+        new SyncStrategy({
+          source: 'remote',
+          target: 'memory',
+          blocking: true
+        })
+      );
+    });
+
+    hooks.afterEach(() => {
+      keyMap = schema = remote = memory = coordinator = null;
+
+      fetchStub.restore();
+    });
+
+    test('Adding a record to the memory source immediately pushes the update to the remote', async function (assert) {
+      assert.expect(4);
+
+      await coordinator.activate();
+
+      fetchStub.withArgs('/planets').returns(
+        jsonapiResponse(201, {
+          data: {
+            id: '12345',
+            type: 'planets',
+            attributes: { name: 'Jupiter', classification: 'gas giant' }
+          }
+        })
+      );
+
+      let planet: Record = {
+        type: 'planet',
+        id: schema.generateId(),
+        attributes: { name: 'Jupiter', classification: 'gas giant' }
+      };
+
+      let createdRecord = await memory.update((t) => t.addRecord(planet));
+      let result = memory.cache.query((q) => q.findRecord(planet));
+
+      assert.deepEqual(result, {
+        type: 'planet',
+        id: planet.id,
+        keys: { remoteId: '12345' },
+        attributes: { name: 'Jupiter', classification: 'gas giant' }
+      });
+      assert.deepEqual(createdRecord, result);
+
+      assert.equal(
+        keyMap.idToKey('planet', 'remoteId', planet.id),
+        '12345',
+        'id mapped to key'
+      );
+      assert.equal(
+        keyMap.keyToId('planet', 'remoteId', '12345'),
+        planet.id,
+        'key mapped to id'
+      );
+    });
+  }
+);
