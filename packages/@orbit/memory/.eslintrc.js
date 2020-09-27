c871b92b3959fe5572fb4fe72776cf417100c002
Dan Gebhardt <dan@cerebris.com>
2020-09-27 13:14:25 -0400

    Streamline eslint settings

--- packages/@orbit/memory/.eslintrc.js
+++ packages/@orbit/memory/.eslintrc.js
@@ -15,9 +15,6 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-empty-function': ['off'],
-    '@typescript-eslint/no-use-before-define': ['off'],
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/explicit-member-accessibility': ['off'],
     '@typescript-eslint/no-explicit-any': ['off'],
     'prefer-const': ['off'],
     'prefer-rest-params': ['off'],
