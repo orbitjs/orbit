71efab47affc88c5044855082b7fd2ac9616d305
Dan Gebhardt <dan@cerebris.com>
2020-11-16 08:25:18 -0500

    Fix RecordPullable and RecordPushable

--- packages/@orbit/records/src/record-source-interfaces/record-pullable.ts
+++ packages/@orbit/records/src/record-source-interfaces/record-pullable.ts
@@ -1,9 +1,11 @@
 import { Pullable } from '@orbit/data';
 import { RecordOperation } from '../record-operation';
+import { RecordQueryResult } from '../record-query';
 import { RecordQueryBuilder } from '../record-query-builder';
 import { RecordQueryExpression } from '../record-query-expression';
 
 export type RecordPullable<ResponseDetails> = Pullable<
+  RecordQueryResult,
   ResponseDetails,
   RecordOperation,
   RecordQueryExpression,
--- packages/@orbit/records/src/record-source-interfaces/record-pushable.ts
+++ packages/@orbit/records/src/record-source-interfaces/record-pushable.ts
@@ -1,8 +1,10 @@
 import { Pushable } from '@orbit/data';
 import { RecordOperation } from '../record-operation';
+import { RecordTransformResult } from '../record-transform';
 import { RecordTransformBuilder } from '../record-transform-builder';
 
 export type RecordPushable<ResponseDetails> = Pushable<
+  RecordTransformResult,
   ResponseDetails,
   RecordOperation,
   RecordTransformBuilder
