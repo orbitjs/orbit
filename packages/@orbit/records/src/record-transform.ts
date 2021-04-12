be12a1e9b51c7bd34876ce0ad9ff1675275e0fed
Dan Gebhardt <dan@cerebris.com>
2021-04-12 09:19:42 -0400

    Remove unnecessary RecordTransformOrOperations interface

--- packages/@orbit/records/src/record-transform.ts
+++ packages/@orbit/records/src/record-transform.ts
@@ -1,16 +1,10 @@
 import { RecordOperation, RecordOperationResult } from './record-operation';
 import { InitializedRecord } from './record';
-import { Transform, TransformOrOperations } from '@orbit/data';
-import { RecordTransformBuilder } from './record-transform-builder';
+import { Transform } from '@orbit/data';
 
 export interface RecordTransform extends Transform<RecordOperation> {
   operations: RecordOperation[];
 }
-
-export type RecordTransformOrOperations = TransformOrOperations<
-  RecordOperation,
-  RecordTransformBuilder
->;
 
 export type RecordTransformResult<T = InitializedRecord> =
   | RecordOperationResult<T>
