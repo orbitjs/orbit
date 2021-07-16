89c2ab180117decb31ff8b9d6fb46bc60c0c331b
Dan Gebhardt <dan@cerebris.com>
2021-07-16 13:28:22 -0400

    JSONAPISource#update should always return an array of results when operations is an array

--- packages/@orbit/jsonapi/src/jsonapi-source.ts
+++ packages/@orbit/jsonapi/src/jsonapi-source.ts
@@ -398,7 +398,7 @@ export class JSONAPISource<
     }
 
     return {
-      data: responses.length > 1 ? data : data[0],
+      data: Array.isArray(transform.operations) ? data : data[0],
       details,
       transforms: [transform, ...transforms]
     };
