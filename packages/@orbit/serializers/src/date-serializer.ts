cd907340d5fe8b52905d43b41a02308fb809f8f7
Edward Faulkner <edward@eaf4.com>
2022-03-15 00:30:47 -0400

    Make date serializer respect ISO 8601
    The default date serializer looks like it intends to follow ISO 8601, and its test says "#serialize returns a date in YYYY-MM-DD form".
    
    But it doesn't always emit YYYY-MM-DD form, because it doesn't insert leading zeros for single digit months and days. This results in dates that aren't strictly conformant to ISO 8601.
    
    This change adds the necessary padding.
    
    This is probably a breaking change.

--- packages/@orbit/serializers/src/date-serializer.ts
+++ packages/@orbit/serializers/src/date-serializer.ts
@@ -2,7 +2,10 @@ import { BaseSerializer } from './base-serializer';
 
 export class DateSerializer extends BaseSerializer<Date, string> {
   serialize(arg: Date): string {
-    return `${arg.getFullYear()}-${arg.getMonth() + 1}-${arg.getDate()}`;
+    let YYYY = arg.getFullYear().toString().padStart(4, '0');
+    let MM = (arg.getMonth() + 1).toString().padStart(2, '0');
+    let DD = arg.getDate().toString().padStart(2, '0');
+    return `${YYYY}-${MM}-${DD}`;
   }
 
   deserialize(arg: string): Date {
